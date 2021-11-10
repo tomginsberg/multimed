fix disease weighting

data/mimic_medaug.py
@@ -55,6 +55,11 @@ class MimicPatientPositivePairDataset(MimicCxrJpgDataset):
         assert not (same_study and diff_study)
         # self.same_disease = same_disease
 
+    def get_label(self, exam):
+        labels = np.array(exam.reindex(self.label_list)[self.label_list]).astype(np.float)
+        labels[np.isnan(labels)] = 0
+        return np.dot(labels, 1 << np.arange(labels.size, dtype='int64')[::-1])
+
     def __getitem__(self, idx):
 
         exam = self.csv.iloc[idx]
@@ -91,8 +96,8 @@ class MimicPatientPositivePairDataset(MimicCxrJpgDataset):
             key_exam = exam
             key_image = query_image
 
-        key_labels = np.array(key_exam.reindex(self.label_list)[self.label_list]).astype(np.float)
-        query_labels = np.array(exam.reindex(self.label_list)[self.label_list]).astype(np.float)
+        key_labels = self.get_label(key_exam)
+        query_labels = self.get_label(exam)
 
         # TODO: add disease
         meta_info = {

modelling/moco.py
@@ -18,7 +18,6 @@ class MoCo(nn.Module):
             self,
             encoder_q: nn.Module,
             encoder_k: nn.Module,
-            meta_keys: list,
             dim: int = 128,
             K: int = 65536,
             m: float = 0.999,
@@ -74,10 +73,11 @@ class MoCo(nn.Module):
         self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
 
         # create the meta_info queue
-        self.meta_keys = meta_keys
         self.queue_meta = dict()
+        self.meta_keys = ['disease', 'id']
         for meta in self.meta_keys:
-            self.register_buffer("queue_" + meta, -1 * torch.ones(K))
+            init_val = -1 * torch.ones(K)
+            self.register_buffer("queue_" + meta, init_val)
             self.queue_meta[meta] = getattr(self, "queue_" + meta)
 
     @torch.no_grad()
@@ -233,7 +233,9 @@ class MoCo(nn.Module):
         _, query_id = meta_info['id']
 
         # [[q1 @ key1, q1 @ key2, ...], [q2 @ key1, q2 @ key2, ...], ...] (N * K)
-        same_disease = any(map(lambda x: x[0] == x[1] == 1, zip(query_disease.unsqueeze(1), self.queue_meta['disease'].unsqueeze(0))))
+        same_disease = (query_disease.int().unsqueeze(1) & self.queue_meta['disease'].int().unsqueeze(0)).bool()
+        # same_disease = any(map(lambda x: x[0] == x[1] == 1, zip(query_disease.unsqueeze(1),\
+        # self.queue_meta['disease'].unsqueeze(0))))
         diff_id = query_id.unsqueeze(1) != self.queue_meta['id'].unsqueeze(0)
 
         hard_neg = diff_id & torch.logical_not(same_disease)